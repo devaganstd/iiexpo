2463257d3be1806a1e3d7c502fcde01c1baab7a6
devagan <140032345+ideaoutstd@users.noreply.github.com>
2024-10-21 17:43:47 +0700

    Initial

---
+++ __settings__.js
@@ -0,0 +1,22 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "1190497.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'deviceTypes': [`webgl2`, `webgl1`],
+    'powerPreference': "default"
+};
+window.SCRIPTS = [ 51196781, 51196790, 51196698, 51196780, 51196703, 51196702, 51196738, 51196843, 51196685, 51196647, 51196690, 51196810, 51196849, 51196718, 51196697, 51196630, 51196667, 51196706, 51196598, 51196708, 51196609, 51196674, 51196748, 51196872, 51196683, 51196603, 51196875, 51196693 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/51196586/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/51196585/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/51196587/1/ammo.js', 'preload' : true},
+];
