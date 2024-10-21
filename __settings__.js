ece84542269ae4019ef94e22a7f4d8da0d97329f
devagan <140032345+ideaoutstd@users.noreply.github.com>
2024-10-21 20:02:25 +0700

    Push

--- __settings__.js
+++ __settings__.js
@@ -1,14 +1,14 @@
 window.ASSET_PREFIX = "";
 window.SCRIPT_PREFIX = "";
-window.SCENE_PATH = "1190497.json";
+window.SCENE_PATH = "1232871.json";
 window.CONTEXT_OPTIONS = {
     'antialias': true,
     'alpha': false,
     'preserveDrawingBuffer': false,
     'deviceTypes': [`webgl2`, `webgl1`],
-    'powerPreference': "default"
+    'powerPreference': "low-power"
 };
-window.SCRIPTS = [ 51196781, 51196790, 51196698, 51196780, 51196703, 51196702, 51196738, 51196843, 51196685, 51196647, 51196690, 51196810, 51196849, 51196718, 51196697, 51196630, 51196667, 51196706, 51196598, 51196708, 51196609, 51196674, 51196748, 51196872, 51196683, 51196603, 51196875, 51196693 ];
+window.SCRIPTS = [ 55552894, 55552886, 55552896, 55552895, 55552882, 55552742, 55552764, 55552842, 55552668, 55552655, 55552710, 55552624, 55552938, 55552588, 55552632, 55552640, 55552679, 55552940, 55552578, 55552936, 55552913, 55552776, 55552584, 55552699, 55552595, 55552859, 55552729, 55552870, 55552855, 55552592, 55552874, 55552673, 55552815, 55552621, 55552591, 55552922, 55552608, 55552730, 55552612, 55552596, 55552931, 55552918, 55552744, 55552812, 55552939, 55552685, 55552934 ];
 window.CONFIG_FILENAME = "config.json";
 window.INPUT_SETTINGS = {
     useKeyboard: true,
@@ -18,5 +18,5 @@ window.INPUT_SETTINGS = {
 };
 pc.script.legacy = false;
 window.PRELOAD_MODULES = [
-    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/51196586/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/51196585/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/51196587/1/ammo.js', 'preload' : true},
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/55552581/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/55552586/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/55552589/1/ammo.js', 'preload' : true},
 ];
